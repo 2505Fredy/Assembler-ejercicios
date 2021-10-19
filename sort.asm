5b7352a9eb1a0efd1e5a93feb4621e0a14200fd1
Fredy Goolberth Quispe Neira <goolberth.neira@gmail.com>
2021-10-19 12:39:14 -0500

    First commit

---
+++ sort.asm
@@ -0,0 +1,34 @@
+.macro swap(%a, %b, %c)
+  addi %c, %a, 0
+  addi %a, %b, 0
+  addi %b, %c, 0
+.end_macro
+.macro print_int(%a)
+  li $a0, %a
+  li $v0, 3
+  syscall
+.end_macro
+
+#4cadena, 1int, 2float, 3double
+
+.text
+  .globl _start
+  _start:
+    la $a0, text
+    li $v0, 4
+    syscall
+    la $a1, array
+    la $a2, sizeArray
+    lw $t0, array
+    addi $a0, $t0, 0
+    li $v0, 1
+    syscall
+    
+    
+    
+    
+    
+.data
+  array: .word 6, 8, 3, 4, 1, 9, 0, 3, 6, 2
+  sizeArray: .word 10
+  text: .asciiz "La lista ordenada es: "
